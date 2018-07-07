d8b0b3157b746dc9b901c5c1819de6c82264640d
David Dashifen Kees <david.kees@engagedc.com>
2018-07-07 07:29:14 -0400

    php 7.1 > 7.0

--- src/Exception.php
+++ src/Exception.php
@@ -3,25 +3,32 @@
 namespace Dashifen\Exception;
 
 use Throwable;
+use ReflectionClass;
+use ReflectionException;
 
 class Exception extends \Exception {
-	public const UNKNOWN_ERROR = 0;
-	
+	const UNKNOWN_ERROR = 0;
+
 	/**
-	 * Exception constructor.
+	 * Exception constructor
+	 *
 	 * Ensures that the $code parameter is this object's UNKNOWN_ERROR or
 	 * on of its children's other constants.
 	 *
 	 * @param string         $message
 	 * @param int            $code
 	 * @param Throwable|null $previous
+	 *
+	 * @throws ReflectionException
 	 */
 	public function __construct($message = "", $code = 0, Throwable $previous = null) {
 		$code = $this->setCode($code);
 		parent::__construct($message, $code, $previous);
 	}
-	
+
 	/**
+	 * setCode
+	 *
 	 * Either returns $code unchanged, or sets it to $default if it is not
 	 * found within this object's (or one of its children's) constants.
 	 *
@@ -29,9 +36,10 @@ class Exception extends \Exception {
 	 * @param int $default
 	 *
 	 * @return int
+	 * @throws ReflectionException
 	 */
 	protected function setCode(int $code, int $default = self::UNKNOWN_ERROR): int {
-		$reflection = new \ReflectionClass($this);
+		$reflection = new ReflectionClass($this);
 		if (!in_array($code, $reflection->getConstants())) {
 			$code = $default;
 		}

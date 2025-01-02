a2014961fc784ab78b28915f31a72d927ad9b22f
David Dashifen Kees <dashifen@dashifen.com>
2025-01-02 12:17:18 -0500

    correcting a deprecation warning; removing unnecessary catch block

--- src/Exception.php
+++ src/Exception.php
@@ -4,7 +4,6 @@ namespace Dashifen\Exception;
 
 use Throwable;
 use ReflectionClass;
-use ReflectionException;
 
 class Exception extends \Exception
 {
@@ -20,7 +19,7 @@ class Exception extends \Exception
      * @param int|string     $code
      * @param Throwable|null $previous
      */
-    public function __construct(string $message = "", $code = 0, Throwable $previous = null)
+    public function __construct(string $message = "", $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $this->setCode($code), $previous);
     }
@@ -38,42 +37,23 @@ class Exception extends \Exception
      */
     protected function setCode($code, int $default = self::UNKNOWN_ERROR): int
     {
-        try {
-            $reflection = new ReflectionClass($this);
-            $constants = $reflection->getConstants();
-            $returnValue = !is_string($code)
-            
-                // if $code is an int, we see if it's in constants.  if so,
-                // we can return it directly.  otherwise, we want to return
-                // the default.  nested ternary statements are a bad code
-                // smell, but this one is pretty straight forward and keeps
-                // the code looking good.
-            
-                ? (in_array($code, $constants) ? $code : $default)
-                
-                // otherwise, if $code is a string, we see if it indexes a
-                // value in our array of $constants and return that value if it
-                // does.  if not, the null coalescing operator will return make
-                // sure we return our default.
-                
-                : $constants[$code] ?? $default;
-        } catch (ReflectionException $exception) {
-            
-            // a ReflectionException is thrown when the class that we're
-            // trying to reflect doesn't exist.  but, since we're reflecting
-            // this class, we know it exists.  in order to avoid IDE related
-            // messages about uncaught exceptions, we'll trigger the following
-            // error, but we also know that we should never get here.
-            
-            trigger_error("Unable to reflect.", E_ERROR);
-        }
+        $reflection = new ReflectionClass($this);
+        $constants = $reflection->getConstants();
+        return !is_string($code)
         
-        // our ReflectionException should never be thrown.  but, because we
-        // don't define a return value in the catch block, some IDEs may flag
-        // $returnValue as not existing.  so, we'll use the null coalescing
-        // operator (again) to make sure our default is returned if, for some
-        // reason, we get here.
-        
-        return $returnValue ?? $default;
+            // if $code is an int, we see if it's in constants.  if so,
+            // we can return it directly.  otherwise, we want to return
+            // the default.  nested ternary statements are a bad code
+            // smell, but this one is pretty straight forward and keeps
+            // the code looking good.
+            
+            ? (in_array($code, $constants) ? $code : $default)
+                
+            // otherwise, if $code is a string, we see if it indexes a
+            // value in our array of $constants and return that value if it
+            // does.  if not, the null coalescing operator will return make
+            // sure we return our default.
+                
+            : $constants[$code] ?? $default;
     }
 }
